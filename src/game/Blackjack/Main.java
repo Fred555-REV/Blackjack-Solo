70a301f3e5166fca896a319d54128574ce993968
Freddy Canido <freddy.canido12@gmail.com>
2021-09-19 13:18:10 -0400

    put the game in main

--- src/game/Blackjack/Main.java
+++ src/game/Blackjack/Main.java
@@ -7,21 +7,7 @@ public class Main {
     public static void main(String[] args) {
 
         Table table = new Table();
-        //Assignment
-        //setup creates dealer if needed/wanted and players
-        //also creates the deck and for the assignment displays all cards drawn
-//        table.setup();
-//        System.out.println(table.getDeck().getCards().size());
-    //Notes
-        //hit seems to be working drawing one
-        //split seems to work, sometimes
-        //TODO add logic to split only when allowed by rules
-
-        //standing changes hand
-        //but the logic for standing seems to not be working
-
-        //TODO fix split decks making stand not work
-        // or maybe change hands is not working properly
-
+        table.setup();
+        table.round(2);
     }
 }
